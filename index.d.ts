261a894b5db6c2411959ae5a9e77d6a28788c2b8
Vagnerlandio Nunes <vagnerlandio@gmail.com>
2021-04-26 01:34:14 -0300

    replace native textinput with ui-kitten input

--- index.d.ts
+++ index.d.ts
@@ -6,60 +6,62 @@
  * Definitions by: Italo Izaac <https://github.com/iiandrade>
  */
 
-import * as React from 'react'
-import { TextInput, TextInputProps } from 'react-native'
+import * as React from "react";
+import { TextInputProps } from "react-native";
+import { Input } from "@ui-kitten/components";
 
 // Type prop of TextInputMask.
 export type TextInputMaskTypeProp =
-    | 'credit-card'
-    | 'cpf'
-    | 'cnpj'
-    | 'zip-code'
-    | 'only-numbers'
-    | 'money'
-    | 'cel-phone'
-    | 'datetime'
-    | 'custom'
+  | "credit-card"
+  | "cpf"
+  | "cnpj"
+  | "zip-code"
+  | "only-numbers"
+  | "money"
+  | "cel-phone"
+  | "datetime"
+  | "custom";
 
 // Option prop of TextInputMask.
 export interface TextInputMaskOptionProp {
-    // Money type.
-    precision?: number
-    separator?: string
-    delimiter?: string
-    unit?: string
-    suffixUnit?: string
-    zeroCents?: boolean
+  // Money type.
+  precision?: number;
+  separator?: string;
+  delimiter?: string;
+  unit?: string;
+  suffixUnit?: string;
+  zeroCents?: boolean;
 
-    // Phone type.
-    withDDD?: boolean
-    dddMask?: string
-    maskType?: 'BRL' | 'INTERNATIONAL'
+  // Phone type.
+  withDDD?: boolean;
+  dddMask?: string;
+  maskType?: "BRL" | "INTERNATIONAL";
 
-    // Datetime type.
-    format?: string
+  // Datetime type.
+  format?: string;
 
-    // Credit card type.
-    obfuscated?: boolean
-    issuer?: 'visa-or-mastercard' | 'diners' | 'amex'
+  // Credit card type.
+  obfuscated?: boolean;
+  issuer?: "visa-or-mastercard" | "diners" | "amex";
 
-    // Custom type.
-    mask?: string
-    validator?: (value: string, settings: TextInputMaskOptionProp) => boolean
-    getRawValue?: (value: string, settings: TextInputMaskOptionProp) => any
-    translation?: { [s: string]: (val: string) => string | null | undefined }
+  // Custom type.
+  mask?: string;
+  validator?: (value: string, settings: TextInputMaskOptionProp) => boolean;
+  getRawValue?: (value: string, settings: TextInputMaskOptionProp) => any;
+  translation?: { [s: string]: (val: string) => string | null | undefined };
 }
 
 // TextInputMask Props
-export interface TextInputMaskProps extends Pick<TextInputProps, Exclude<keyof TextInputProps, 'onChangeText'>> {
-    type: TextInputMaskTypeProp
-    options?: TextInputMaskOptionProp
-    checkText?: (previous: string, next: string) => boolean
-    onChangeText?: (text: string, rawText?: string) => void
-    refInput?: (ref: any) => void
-    customTextInput?: any
-    customTextInputProps?: Object
-    includeRawValueInChangeText?: boolean
+export interface TextInputMaskProps
+  extends Pick<TextInputProps, Exclude<keyof TextInputProps, "onChangeText">> {
+  type: TextInputMaskTypeProp;
+  options?: TextInputMaskOptionProp;
+  checkText?: (previous: string, next: string) => boolean;
+  onChangeText?: (text: string, rawText?: string) => void;
+  refInput?: (ref: any) => void;
+  customTextInput?: any;
+  customTextInputProps?: Object;
+  includeRawValueInChangeText?: boolean;
 }
 
 // TextInputMask Component
@@ -70,37 +72,37 @@ export class TextMask extends React.Component<TextInputMaskProps> {}
 
 // MaskService
 export namespace MaskService {
-    function toMask(
-        type: string,
-        value: string,
-        options?: TextInputMaskOptionProp
-    ): string
-    function toRawValue(
-        type: string,
-        maskedValue: string,
-        options?: TextInputMaskOptionProp
-    ): string
-    function isValid(
-        type: string,
-        value: string,
-        options?: TextInputMaskOptionProp
-    ): boolean
+  function toMask(
+    type: string,
+    value: string,
+    options?: TextInputMaskOptionProp
+  ): string;
+  function toRawValue(
+    type: string,
+    maskedValue: string,
+    options?: TextInputMaskOptionProp
+  ): string;
+  function isValid(
+    type: string,
+    value: string,
+    options?: TextInputMaskOptionProp
+  ): boolean;
 }
 
 // TextInputMaskMethods
 export class TextInputMaskMethods {
-    getElement(): TextInput
-    getRawValue(): string
-    isValid(): boolean
+  getElement(): Input;
+  getRawValue(): string;
+  isValid(): boolean;
 }
 
 // TextInputMasked
-export type TextInputMasked = TextInputMaskMethods | null
+export type TextInputMasked = TextInputMaskMethods | null;
 
 // TextMaskMethods
 export class TextMaskMethods {
-    getElement(): TextInput
+  getElement(): Input;
 }
 
 // TextMaskInstance
-export type TextMaskInstance = TextMaskMethods | null
+export type TextMaskInstance = TextMaskMethods | null;
